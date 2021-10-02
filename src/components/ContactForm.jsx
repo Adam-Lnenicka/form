5db011d659b814a88064eeb6262b1b37e50a2824
Adam Lnenicka <adam.lnenicka123@gmail.com>
2021-10-02 12:35:35 +0100

    styling finished

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -7,18 +7,18 @@ import * as Yup from 'yup';
 const SignupSchema = Yup.object().shape({
     //yup validation - here we set criteria what we want the user to type
   firstName: Yup.string()
-    .min(2, 'Too Short!')
-    .max(50, 'Too Long!')
-    .required('Required'),
+    .min(2, 'The name seems to be too short')
+    .max(50, 'The entry seems to be too long')
+    .required('Your name is required'),
     //it is a simple form so we assume that all information is required to gather from the user
 
-  email: Yup.string().email('Invalid email').required('Required'),
+  email: Yup.string().email('The email is not valid').required('Please type your email'),
   //for email we need the @ symnbol so that is why we email('Invalid email')
 
   notes: Yup.string()
-  .min(2, 'Too Short!')
-  .max(300, 'Too Long!')
-  .required('Required'),
+  .min(2, 'Please write us a longer message')
+  .max(300, 'Please write a shorter message')
+  .required('Let us know how we can help'),
 });
 
 const ContactForm = () => {
@@ -27,12 +27,24 @@ const ContactForm = () => {
     // we are setting state where needed so we can change it later
 
     const handleSubmit = (e) => {
-        e.preventDefault();
         setSubmitted(true);
     };
 
+    const handleCount = (e) => {
+        // This function changes the state of the counter so it reflects the number of characters. 
+        // It looks like a bit too complex way to write this - normally I would simply use 
+        // onChange={() => { setCount(e.target.value.length ) }. However, this function does not work as 
+        // Formik handles value itself and using value here breaks the code.
+        // For whatever reason though onKeyDown works with some modifications.
 
-
+        const x = e.keyCode;
+        // selecting backspace key
+        if (x === 8 && count >= 1) { setCount(count - 1) }
+        // if we press backspace key, count decreases
+        else if (x === 8 && count === 0) { setCount(0) }
+        else { setCount(e.target.value.length + 1) }
+        // if we type text count increases
+    }
 
 return (
   <div className ="form">
@@ -45,38 +57,41 @@ return (
       }}
     //   here we define the values that we collect
       validationSchema={SignupSchema}
-      onSubmit={values => {
-        // same shape as initial values
-        console.log(values);
-      }}
+    //   onSubmit={values => {
+    //     console.log(values);
+    //   }}
+      onSubmit={handleSubmit}
+    //   if valifation goes through, we call the handleSubmit function onSubmit
     >
       {({ errors, touched }) => (
-        <Form>
-            <div className="form__content">
+        <Form className="form__content">
                 {/* I decided to use BEM here which makes SCSS well targetted and readable, 
                 possibly not necessary for a project foor this scale but a good practice. */}
-                <Field name="firstName" />
-                    {errors.firstName && touched.firstName ? (
-                        <div>{errors.firstName}</div>
-                    ) : null}
-                    {/* this is linked to Yup. if the validation does not go through an error will be displayed */}
-                <Field name="email" type="email" />
-                    {errors.email && touched.email ? <div>{errors.email}</div> : null}
-                    <button type="submit">Submit</button>
-                <Field component="textarea" name="notes" placeholder="notes" onKeyDown={(e) => { handleCount(e)}} />
+                <div className="form__contact-field-box">
+                    <div>
+                        <Field name="firstName" className="form__input" placeholder="Full Name"/>
+                        {errors.firstName && touched.firstName ? (<div className="form__error-message">{errors.firstName}</div>) : null}
+                        {/* this is linked to Yup. if the validation does not go through an error will be displayed */}
+                    </div>
+                    <div>
+                        <Field name="email" type="email" placeholder="Email" className="form__input" />
+                        {errors.email && touched.email ? <div className="form__error-message">{errors.email}</div> : null}
+                    </div>
+                </div>
+                <Field component="textarea" name="notes" className="form__input form__textarea" placeholder="How can we help you today?" onKeyDown={(e) => { handleCount(e)}} />
                     {/* apart from collecting the data we are tryggering the increased count by typing  */}
                     {/* We also need to convert the input to textarea */}
-                    {count}
-                    {errors.lastName && touched.lastName ? (
-                        <div>{errors.lastName}</div>
-                    ) : null}
-            </div>
+                <div className="form__counter">Number of characters: {count}</div>
+
+                    {errors.notes && touched.notes ? (<div className="form__error-message">{errors.notes}</div> ) : null}
+                <button type="submit">Submit</button>
+
         </Form>
       )}
-    <h3>{submitted ? 'Thank you for contacting us, we will get back to you soon!' : null}</h3>
+    </Formik>
+    <p>{submitted ? <div className="form__submit-message">Message submitted, we will get back to you soon!</div> : null}</p>
     {/* if the form is validated and successfully submitted, we will display a message to the user that the form was submitted,
     if form is not validated, the message will not be diaplayed */}
-    </Formik>
   </div>
 )
 };
